43eca24a26dab1edda184fc17b41fc3495f3a8dd
Ezra Parker <ezra@cfgrok.com>
2009-11-12 01:08:45 +0000

    Corrected case-sensitivity issues in ModelGlue.xml of usermanagement actionpack Corrected case-sensitivity issues, typos in MySQL install script & added missing insert statements

--- modelglueactionpacks/usermanagement/install/usermanagementMYSQL.sql
+++ modelglueactionpacks/usermanagement/install/usermanagementMYSQL.sql
@@ -8,9 +8,9 @@ MySQL - 5.0.45-community-nt : Database - usermanangement
 
 /*!40101 SET SQL_MODE=''*/;
 
-create database if not exists `usermanangement`;
+create database if not exists `usermanagement`;
 
-USE `usermanangement`;
+USE `usermanagement`;
 
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
@@ -26,11 +26,15 @@ CREATE TABLE `group` (
   PRIMARY KEY  (`GroupID`)
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 
-/*Table structure for table `groupsecuredmodelglueeventrelationship` */
+INSERT INTO `group` (GroupID,Name)
+VALUES (1,'Administrators'),
+	(5,'User Administrators');
 
-DROP TABLE IF EXISTS `groupsecuredmodelglueeventrelationship`;
+/*Table structure for table `groupSecuredModelGlueEventRelationship` */
 
-CREATE TABLE `groupsecuredmodelglueeventrelationship` (
+DROP TABLE IF EXISTS `groupSecuredModelGlueEventRelationship`;
+
+CREATE TABLE `groupSecuredModelGlueEventRelationship` (
   `Id` int(11) NOT NULL auto_increment,
   `GroupID` int(11) default NULL,
   `EventID` int(11) NOT NULL,
@@ -40,15 +44,43 @@ CREATE TABLE `groupsecuredmodelglueeventrelationship` (
   CONSTRAINT `gsmer_groupid` FOREIGN KEY (`GroupID`) REFERENCES `group` (`GroupID`)
 ) ENGINE=InnoDB AUTO_INCREMENT=100 DEFAULT CHARSET=utf8;
 
-/*Table structure for table `securedmodelglueevent` */
+INSERT INTO `groupSecuredModelGlueEventRelationship` (Id,GroupID,EventID)
+VALUES ('88','1','8'),
+	('89','1','5'),
+	('90','1','7'),
+	('91','1','13'),
+	('92','1','11'),
+	('93','1','9'),
+	('94','1','12'),
+	('95','1','4'),
+	('96','1','2'),
+	('97','1','1'),
+	('98','1','3'),
+	('99','1','6');
 
-DROP TABLE IF EXISTS `securedmodelglueevent`;
+/*Table structure for table `securedModelGlueEvent` */
 
-CREATE TABLE `securedmodelglueevent` (
+DROP TABLE IF EXISTS `securedModelGlueEvent`;
+
+CREATE TABLE `securedModelGlueEvent` (
   `EventId` int(11) NOT NULL auto_increment,
   `Name` varchar(500) NOT NULL,
   PRIMARY KEY  (`EventId`)
 ) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8;
+
+INSERT INTO `securedModelGlueEvent` (EventId,Name)
+VALUES ('1','userManagement.user.list'),
+	('2','userManagement.user.edit'),
+	('3','userManagement.user.save'),
+	('4','userManagement.user.delete'),
+	('5','userManagement.group.list'),
+	('6','userManagement.group.edit'),
+	('7','userManagement.group.save'),
+	('8','userManagement.group.delete'),
+	('9','userManagement.securedModelGlueEvent.list'),
+	('11','userManagement.securedModelGlueEvent.edit'),
+	('12','userManagement.securedModelGlueEvent.save'),
+	('13','userManagement.securedModelGlueEvent.delete');
 
 /*Table structure for table `user` */
 
@@ -59,16 +91,20 @@ CREATE TABLE `user` (
   `Username` varchar(50) NOT NULL,
   `Password` varchar(50) NOT NULL,
   `emailAddress` varchar(200) NOT NULL,
-  `annonymousAccount` bit(1) NOT NULL,
+  `anonymousAccount` bit(1) NOT NULL,
   PRIMARY KEY  (`UserId`),
   UNIQUE KEY `user_unique_username` (`Username`)
 ) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
 
-/*Table structure for table `usergrouprelationship` */
+INSERT INTO `user` (UserId,Username,Password,emailAddress,anonymousAccount)
+VALUES ('1','anon','3mous3','blah','1'),
+	('12','admin','admin','admin@test.tst','1');
 
-DROP TABLE IF EXISTS `usergrouprelationship`;
+/*Table structure for table `userGroupRelationship` */
 
-CREATE TABLE `usergrouprelationship` (
+DROP TABLE IF EXISTS `userGroupRelationship`;
+
+CREATE TABLE `userGroupRelationship` (
   `userGroupRelationshipId` int(11) NOT NULL auto_increment,
   `UserId` int(11) NOT NULL,
   `GroupId` int(11) NOT NULL,
@@ -78,6 +114,9 @@ CREATE TABLE `usergrouprelationship` (
   CONSTRAINT `ugr_group` FOREIGN KEY (`GroupId`) REFERENCES `group` (`GroupID`),
   CONSTRAINT `ugr_user` FOREIGN KEY (`UserId`) REFERENCES `user` (`UserId`)
 ) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=utf8;
+
+INSERT INTO `userGroupRelationship` (userGroupRelationshipId,UserId,GroupId)
+VALUES ('31','12','1');
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
